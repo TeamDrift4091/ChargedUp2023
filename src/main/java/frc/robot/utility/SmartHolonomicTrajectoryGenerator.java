c2af800dd97a4ca7c07a4338ad7a3f906f428c42
StephenDay1 <57306301+StephenDay1@users.noreply.github.com>
2023-02-21 16:17:02 -0400

    Update to BullBotsLib-2023.2.2

--- src/main/java/frc/robot/utility/SmartHolonomicTrajectoryGenerator.java
+++ src/main/java/frc/robot/utility/SmartHolonomicTrajectoryGenerator.java
@@ -12,7 +12,6 @@ import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.subsystems.Drivetrain;
