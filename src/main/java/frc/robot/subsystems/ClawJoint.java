eeff1ed6a9ec70f61891b23e45489846f0b1a3ac
24ginemar1 <24ginemar1@cms.edu.do>
2023-04-09 22:45:52 -0400

    missing import

--- src/main/java/frc/robot/subsystems/ClawJoint.java
+++ src/main/java/frc/robot/subsystems/ClawJoint.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.AbsoluteEncoder;
-
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class ClawJoint{
