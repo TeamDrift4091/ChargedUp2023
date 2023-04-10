5ab76eed54df46b388a7d761720962799c45f540
24ginemar1 <24ginemar1@cms.edu.do>
2023-04-09 23:52:07 -0400

    motor changes

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -6,17 +6,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Claw extends SubsystemBase {
     private WPI_TalonFX motor1;
     private WPI_TalonFX motor2;
+    private WPI_TalonFX motor3;
+    private WPI_TalonFX motor4;
 
-    public Claw(int motor1ID, int motor2ID) {
+    public Claw(int motor1ID, int motor2ID, int motor3ID, int motor4ID) {
         motor1 = new WPI_TalonFX(motor1ID);
         motor2 = new WPI_TalonFX(motor2ID);
+        motor3 = new WPI_TalonFX(motor3ID);
+        motor4 = new WPI_TalonFX(motor4ID);
+
+
 
         motor1.configFactoryDefault();
         motor2.configFactoryDefault();
+        motor3.configFactoryDefault();
+        motor4.configFactoryDefault();
     }
 
     public void setMotorSpeed(double speed) {
         motor1.set(speed);
         motor2.set(speed);
+        motor3.set(speed);
+        motor4.set(speed);
     }
 }
