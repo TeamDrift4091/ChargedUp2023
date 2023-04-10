1781153e5f61b208a926f3153bb93e7a2a3fdf80
24ginemar1 <24ginemar1@cms.edu.do>
2023-04-09 23:19:52 -0400

    Claw subsystem
    created the claw subsystem

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -1,17 +1,22 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Claw extends SubsystemBase {
-  /** Creates a new Claw. */
-  public Claw() {}
+    private WPI_TalonFX motor1;
+    private WPI_TalonFX motor2;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public Claw(int motor1ID, int motor2ID) {
+        motor1 = new WPI_TalonFX(motor1ID);
+        motor2 = new WPI_TalonFX(motor2ID);
+
+        motor1.configFactoryDefault();
+        motor2.configFactoryDefault();
+    }
+
+    public void setMotorSpeed(double speed) {
+        motor1.set(speed);
+        motor2.set(speed);
+    }
 }
--- src/main/java/frc/robot/subsystems/ClawJoint.java
+++ src/main/java/frc/robot/subsystems/ClawJoint.java
@@ -1,6 +1,4 @@
 package frc.robot.subsystems;
-
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
