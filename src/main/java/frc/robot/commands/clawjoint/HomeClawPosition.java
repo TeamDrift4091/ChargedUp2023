cbbbae9d44d96aae6241d3fdb20bdcb37c763078
24ginemar1 <24ginemar1@cms.edu.do>
2023-04-12 10:10:24 -0400

    HomeClawPosition

--- src/main/java/frc/robot/commands/clawjoint/HomeClawPosition.java
+++ src/main/java/frc/robot/commands/clawjoint/HomeClawPosition.java
@@ -1,32 +1,21 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.clawjoint;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ClawJoint;
 
 public class HomeClawPosition extends CommandBase {
-  /** Creates a new HomeClawPosition. */
-  public HomeClawPosition() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+    private final ClawJoint clawJoint;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    private static final double HOME_ANGLE = Math.PI / 2.0;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+    public HomeClawPosition(ClawJoint clawJoint) {
+        this.clawJoint = clawJoint;
+        addRequirements(clawJoint);
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    @Override
+    public void execute() {
+        clawJoint.setAngle(HOME_ANGLE);
+    }
 }
+
